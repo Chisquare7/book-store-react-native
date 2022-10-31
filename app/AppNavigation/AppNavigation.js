3c830bcd093d0878779fe210881fbfa1940e3576
Chisquare7 <chijiokechibuike.f@gmail.com>
2022-10-31 11:31:18 +0100

    Commit of former progress

--- app/AppNavigation/AppNavigation.js
+++ app/AppNavigation/AppNavigation.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import HomeScreen from "../screens/HomeScreen";
+import DetailedBook from "../screens/DetailedBook";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,7 +13,12 @@ export default function AppNavigation({ AppState }) {
 			<Stack.Navigator>
 				<Stack.Screen name="Home" options={{headerShown: false}}>
 					{(props) => <HomeScreen {...props} AppState={AppState} />}
-				</Stack.Screen>
+                </Stack.Screen>
+                
+                <Stack.Screen name="DetailedBook" options={{headerShown: true}}>
+					{(props) => <DetailedBook {...props} AppState={AppState} />}
+                </Stack.Screen>
+                
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
